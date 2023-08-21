f8b53094566f00a28a819f97578afd2d9541c150
Muhirwa Thierry <mithierry1010@gmail.com>
2023-08-21 15:51:31 +0200

    String arrays and switch in branching.

--- JavaClass/src/Strings.java
+++ JavaClass/src/Strings.java
@@ -1,5 +1,5 @@
 /*************************************
-* Stirngs in java
+* Strings in java
 *************************************/
 public class Strings {
     public static void main(String[] args)
@@ -11,7 +11,6 @@ public class Strings {
        for (int i = 0; i < str1.length(); i++)
            System.out.println(str1.charAt(i));
 
-       System.out.println(str3);
        System.out.println(str1.equals(str2));
        System.out.println(str1.length());
        System.out.println(str2.compareTo(str1));
---
+++ JavaClass/src/Switching.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Switching {
+    public static void main(String[] args)
+    {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter the command: ");
+        String command = reader.nextLine();
+
+        // Branching using switch.
+        switch (command)
+        {
+            case "connect":
+                System.out.println("Connecting...");
+                break;
+            case "disconnect":
+                System.out.println("Disconnecting...");
+                break;
+            case "cancel":
+                System.out.println("Canceling...");
+                break;
+            default:
+                System.out.println("This is command is not known.");
+        }
+
+        // Branching using if and else
+        if (command.equals("connect"))
+            System.out.println("Connecting...");
+
+        else if (command.equals("disconnect"))
+            System.out.println("Disconnecting...");
+
+        else if (command.equals("cancel"))
+            System.out.println("Canceling...");
+        else
+            System.out.println("This command is not recognized in the system.");
+    }
+}
